494fffc9f7866f9abcfcae7224b39b18b9828432
mallikarjun432 <45162473+mallikarjun432@users.noreply.github.com>
2019-09-09 12:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!malli')
+  res.send('Hello World!malli123')
 })
 
 app.listen(3000, function () {
